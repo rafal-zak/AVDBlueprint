2e186c6db5995e763629e7a5b2df48bd98faafcb
Robert Smith <61853161+robsmi-msfte@users.noreply.github.com>
2021-09-16 15:57:32 -0400

    Update Remove-AzAvdBpDeployment.ps1
    Updated the PurgeKeyVault code to check for the Az.KeyVault PowerShell module before running.  If not found, the script installs it.

--- Examples & Samples/Remove-AzAvdBpDeployment.ps1
+++ Examples & Samples/Remove-AzAvdBpDeployment.ps1
@@ -109,16 +109,16 @@ $RemovalScope | ForEach-Object {
         }
     }
    
-        Write-Verbose "Now purging key vault (will install Az.Keyvault PowerShell module if not already installed"
+        Write-Verbose "Now purging key vault (will install Az.Keyvault PowerShell module if not already installed)"
+    if ($PurgeKeyVault) {
         if (-not(Get-Module -Name Az.Keyvault)) {
-        Install-Module 'Az.Keyvault' -Force
+            Install-Module 'Az.Keyvault' -Force
         }
-        if ($PurgeKeyVault) {
-            $KeyVaultToPurge = Get-AzKeyVault -ResourceGroupName $RemovalScope.ResourceGroupName
-            Write-Verbose "Found '$($KeyVaultToPurge.VaultName)' Key Vault"
-            Remove-AzKeyVault -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
-            Remove-AzKeyVault -InRemovedState -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
-        }
+        $KeyVaultToPurge = Get-AzKeyVault -ResourceGroupName $RemovalScope.ResourceGroupName
+        Write-Verbose "Found '$($KeyVaultToPurge.VaultName)' Key Vault"
+        Remove-AzKeyVault -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
+        Remove-AzKeyVault -InRemovedState -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
+    }
 
     if($PSCmdlet.ShouldProcess($_.ResourceGroupName, "Remove ResourceGroup")){
         $_ | Remove-AzResourceGroup -Force -AsJob
